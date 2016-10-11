2885e9f586d2685dfefc30bff0ec8af2ed5284fc
Ryan Marcus <ryan@rmarcus.info>
2016-10-11 16:02:52 -0400

    added GPLv3

--- src/main/java/edu/brandeis/ggen/GGenException.java
+++ src/main/java/edu/brandeis/ggen/GGenException.java
@@ -1,3 +1,24 @@
+// { begin copyright } 
+// Copyright Ryan Marcus 2016
+// 
+// This file is part of poingnard.
+// 
+// poingnard is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// poingnard is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with poingnard.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// { end copyright } 
+ 
+ 
 package edu.brandeis.ggen;
 
 public class GGenException extends Exception {
